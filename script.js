c368635f2f5dc9927730f9d674b4f12fd01a816d
sp3ncerlan <lansp329@gmail.com>
2023-01-09 17:49:37 -0600

    quick update

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ let sizeOfGrid = 16;
 // javascript
 
 const container = document.querySelector(".container")
+const resetButton = document.querySelector("button")
 
 const createGrid = (amtOfGrids) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
+
   for (let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div')
     row.classList.add('grid-row')
@@ -21,8 +25,24 @@ const createGrid = (amtOfGrids) => {
       })
       row.appendChild(gridBox)
     }
-    container.appendChild(row)
+    wrapper.appendChild(row)
   }
+  container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+resetButton.addEventListener('click', () => {
+  let userSize = Number(prompt("What dimensions do you want for the new grid?"))
+
+  while (userSize > 100) {
+    userSize = Number(prompt("Pick a smaller number (<100)"))
+  }
+
+  const wrapper = document.querySelector('.wrapper')
+  
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
+  }
+})
