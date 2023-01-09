a2931db907d061eacb5fc47694056a1fe82bdaa6
sp3ncerlan <lansp329@gmail.com>
2023-01-09 16:42:04 -0600

    configured files so that there is a grid based on the asked size + change in background color when hovered over

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // functions
-
+let sizeOfGrid = 16;
 // javascript
 
-const container = document.getElementById("container")
-for (let row = 0; row < 16; row++) {
-  for (let col = 0; col < 16; col++) {
-    const gridItem = document.createElement("div")
-    gridItem.classList.add("grid-item")
-    container.appendChild(gridItem)
+const container = document.querySelector(".container")
+
+const createGrid = (amtOfGrids) => {
+  for (let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement('div')
+    row.classList.add('grid-row')
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960 / sizeOfGrid
+      const gridBox = document.createElement('div')
+      gridBox.classList.add('grid-box')
+      gridBox.style.width = `${widthAndHeight}px`
+      gridBox.style.height = `${widthAndHeight}px`
+      // adding event listener for mouse enter
+      gridBox.addEventListener('mouseenter', () => {
+        gridBox.style.backgroundColor = 'black'
+      })
+      row.appendChild(gridBox)
+    }
+    container.appendChild(row)
   }
 }
+
+createGrid(sizeOfGrid)
